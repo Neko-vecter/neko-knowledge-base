7c6d502c9cfd8f52a14bc60ebbc39366eb1bd3b4
Neko-vecter <vecterfang@icloud.com>
2026-05-27 03:18:49 -0400

    add tex support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import {default as custom_code_themes} from './src/theme/prism-custom-code-themes';
 
 const config: Config = {
@@ -54,6 +57,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: {
           showReadingTime: false,
